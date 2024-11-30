04eadcae8f0c1d22b3affb0fe730ad7c3d28efb2
Demilson Junior <45902044+JuniorBo@users.noreply.github.com>
2024-11-29 23:51:24 -0500

    Delete Portfolio directory

--- Portfolio/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-export default function Logo() {
-  return (
-    <Image src="/logo.png" alt="Logo" width={150} height={150} />
-  )
-}
--- Portfolio/components/ui/Card.js
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, className = '' }) => {
-    return (
-      <div className={`bg-white dark:bg-gray-800 rounded-xl shadow-lg overflow-hidden ${className}`}>
-        {children}
-      </div>
-    )
-  }
-  
-  export default Card
